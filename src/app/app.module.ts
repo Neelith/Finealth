652e6c200d34a221c5620a0f52027873c6f3ea31
Alessandro Averna <neelith@hotmail.it>
2023-05-27 16:38:46 +0200

    added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,7 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { MaterialModule } from './material/material.module';
 import { NgxIndexedDBModule } from 'ngx-indexed-db';
 import { indexedDbConfig } from './indexedDb/indexedDbConfig';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
     declarations: [
@@ -21,7 +22,13 @@ import { indexedDbConfig } from './indexedDb/indexedDbConfig';
         MaterialModule,
         BrowserAnimationsModule,
         NavbarComponent,
-        NgxIndexedDBModule.forRoot(indexedDbConfig)
+        NgxIndexedDBModule.forRoot(indexedDbConfig),
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ]
 })
 export class AppModule { }
