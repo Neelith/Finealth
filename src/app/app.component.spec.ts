387aca41610935093799b9ed623e372a17fc59a1
Alessandro Averna <neelith@hotmail.it>
2023-05-28 15:41:28 +0200

    changed name to Finealth

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'NeroFinealthClient'`, () => {
+  it(`should have as title 'Finealth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('NeroFinealthClient');
+    expect(app.title).toEqual('Finealth');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('NeroFinealthClient app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Finealth app is running!');
   });
 });
